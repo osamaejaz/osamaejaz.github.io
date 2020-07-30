3dc0995ba0aac6ea32c6200a1681eacb54c176c7
osama.e@zetwerk.com <osama.e@zetwerk.com>
2020-07-30 11:29:33 +0530

    added theme

---
+++ src/components/footer.js
@@ -0,0 +1,6 @@
+// import PropTypes from "prop-types"
+import React from "react"
+
+const Footer = () => <footer></footer>
+
+export default Footer
--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,25 @@
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+const toggleThemeModes = event => {
+  const currentTheme = document.documentElement.getAttribute("data-theme")
+  if (currentTheme === "dark") {
+    document.documentElement.setAttribute("data-theme", "light")
+  } else {
+    document.documentElement.setAttribute("data-theme", "dark")
+  }
+}
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
+  <header className="row">
+    <button
+      type="button"
+      className="btn btn-primary"
+      onClick={toggleThemeModes}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+      Single toggle
+    </button>
   </header>
 )
 
